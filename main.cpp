b2af269403782f71bd9dde08c2f6a18e4097f090
Tim Meusel <Tim Meusel>
2015-05-13 17:00:29 +0200

    echo employee list

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main()
   tMitarbeiter mitarbeiterListe[100];
   int anzahl = 0;
   tMitarbeiter neuerMitarbeiter = mitarbeiterEinlesen();
-  mitarbeiterHinzufuegen(anzahl, neuerMitarbeiter, mitarbeiterListe);
+  anzahl = mitarbeiterHinzufuegen(anzahl, neuerMitarbeiter, mitarbeiterListe);
+  mitarbeiterlisteAusgeben(mitarbeiterListe, anzahl);
 }
